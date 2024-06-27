b84ef8d1304d35cda287f30d086bb25feb597120
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2024-06-27 14:39:24 +0530

    Update pointer.c

--- C/pointer.c
+++ C/pointer.c
@@ -3,14 +3,14 @@
   *p address of a variable
   */
 int main(){
-    // int i=8;           
-    // int *p;
-    // p=&i;
+    int i=8;           
+    int *p;
+    p=&i;
 
-    // printf("%d \n",*p);//value 
-    // printf("%d \n",p);// address
-    // printf("%d \n",&i);  //&i=p  address
-    // printf("%d \n",i); //value
+    printf("%d \n",*p);//value 
+    printf("%d \n",p);// address
+    printf("%d \n",&i);  //&i=p  address
+    printf("%d \n",i); //value
 
 
 
